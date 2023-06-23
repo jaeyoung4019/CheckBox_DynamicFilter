46d356d11a9e1934acba731b7ec6c27331e8835f
로제커엽 <135151752+jaeyoung4019@users.noreply.github.com>
2023-06-23 10:16:28 +0900

    Add files via upload

--- Filter/DynamicFilter.tsx
+++ Filter/DynamicFilter.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, {createContext, useEffect, useMemo, useState} from "react";
 import Checkbox from "./CheckBox";
 import useForm from "../../Hooks/useForm";
 
@@ -12,24 +12,17 @@ interface variableInterface {
 }
 
 const DynamicFilter = ({ schVariable, selectMenu }: schVariableInterface) => {
-    const testArray1 = ["dynamic1", "dynamic2", "dynamic3", "dynamic4", "dynamic5" , "dynamic6" , "dynamic7" , "dynamic8"];
+    const testArray1 = ["dynamic1", "dynamic2", "dynamic3", "dynamic4", "dynamic5" , "dynamic6" , "dynamic7" , "dynamic8" , "dynamic348" , "dynamic82" , "dynamic812223" , "dynamic8123" , "dynamic81244" , "dynamic338" , "dynamic811" ,"dynamic558" ];
     const testArray2 = ["dynamic1", "dynamic2", "dynamic3"];
 
     const [array, setArray] = useState(testArray1);
 
-    // 변수 변경 테스트용
+    //변수 변경 테스트용
     useEffect(() => {
-        setArray(testArray1.splice(0 ,2));
+        setArray(testArray1.slice(0 ,5));
     }, [schVariable]);
 
     const initVariable = {};
-
-    // const slice: ({start , end}: {start: number , end: number}) => {start: number , end: number} = ({start , end} : {start: number , end: number}) => {
-    //    end += 2;
-    //    return {start: start, end: end}
-    // }
-
-
 
     const [variable, onChangeVariable, emptyCheck, onBlur] = useForm(initVariable);
     const [selectFilter, setSelectFilter] = useState<string[]>([]);
@@ -54,36 +47,37 @@ const DynamicFilter = ({ schVariable, selectMenu }: schVariableInterface) => {
 
     const [count , setCount] = useState<number>(2);
 
-    const sliceClosure = (function () {
-        let plus = 2
+    const sliceClosure = useMemo( () => (function () {
+        let plus = 5
         return function () {
-            //plus + number
-            ++plus;
-            // plus + 2 는 안됨
-            return ++plus;
-            // return function () {
-            //     //plus + number
-            //     return ++plus;
-            // }
+            plus += 5;
+            if (testArray1.length - 4 <=  plus) {
+                plus = testArray1.length;
+                return plus
+            } else {
+                return plus;
+            }
         }
-    }())
+    }()) , [])
 
-    const filterLengthFunc = () => {
-        // const count = sliceClosure()
-        // console.log(count)
+    const filterLengthFuncState = () => {
         setCount( (value) => {
-            if (testArray1.length - 1 <= value) {
+            if (testArray1.length - 4 <= value) {
                 return testArray1.length
             } else
-            return value + 2
+            return value + 5
         })
-        // set 사용해서 렌더링 새로 되니까 클로저를 해도 즉시 실행함수가 다시 호출 되서 초기화 됨.
         console.log(count)
     }
 
-    useEffect(() => {
-        setArray(testArray1.splice(0 ,count));
-    }, [count])
+    const filterLengthClosure = () => {
+        const countClosure = sliceClosure();
+        setArray( testArray1.slice(0 ,countClosure));
+    }
+
+    // useEffect(() => {
+    //     setArray(testArray1.splice(0 ,count));
+    // }, [count])
 
     return (
         <>
@@ -95,7 +89,7 @@ const DynamicFilter = ({ schVariable, selectMenu }: schVariableInterface) => {
                 ?.map(value => {
                     return <p key={value}>{value}</p>;
                 })}
-            <button onClick={filterLengthFunc} >more</button>
+            <button onClick={filterLengthClosure} >more</button>
         </>
     );
 };
